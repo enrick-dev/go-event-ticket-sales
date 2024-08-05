c0626624f6de4f2d450f36ce560d9a058a6e2360
enrick-dev <enricksantos03@gmail.com>
2024-08-04 21:50:50 -0300

    Criado Validate do Spot e Event e Adicionar Spot

--- internal/events/domain/events.go
+++ internal/events/domain/events.go
@@ -1,7 +1,15 @@
 package domain
 
 import (
+	"errors"
 	"time"
+)
+
+var (
+	ErrEventNameRequired = errors.New("event name is required")
+	ErrEventDateFuture   = errors.New("event date must be in the future")
+	ErrEventCapacityZero = errors.New("event capacity must be greater than zero")
+	ErrEventPriceZero    = errors.New("event price must be greater than zero")
 )
 
 type Rating string
@@ -28,4 +36,33 @@ type Event struct {
 	PartnerID    int
 	Spots        []Spot
 	Tickets      []Ticket
+}
+
+func (e Event) Validate() error {
+	if e.Name == "" {
+		return ErrEventNameRequired
+	}
+
+	if e.Date.Before(time.Now()) {
+		return ErrEventDateFuture
+	}
+
+	if e.Capacity <= 0 {
+		return ErrEventCapacityZero
+	}
+
+	if e.Price <= 0 {
+		return ErrEventPriceZero
+	}
+	return nil
+}
+
+func (e *Event) AddSpot(name string) (*Spot, error) {
+	spot, err := NewSpot(e, name)
+
+	if err != nil {
+		return nil, err
+	}
+	e.Spots = append(e.Spots, *spot)
+	return spot, nil
 }
