2920a414b49abd451e6b435d9ed783a78474958d
enrick-dev <enricksantos03@gmail.com>
2024-08-11 15:55:04 -0300

    adicionado event_repository e criado funcion de crate Spot, Ticket e Finds

---
+++ internal/infra/repository/event_repository_mysql.go
@@ -0,0 +1,78 @@
+package repository
+
+import (
+	"database/sql"
+
+	"github.com/enrick-dev/go-event-ticket-sales.git/internal/events/domain"
+)
+
+type mysqlEventRepository struct {
+	db *sql.DB
+}
+
+// func NewMysqlEventRepository(db *sql.DB) (domain.EventRepository, error) {
+// 	return &mysqlEventRepository{db: db}, nil
+// }
+
+func (r *mysqlEventRepository) CreateSpot(spot *domain.Spot) error {
+	query := `
+		INSERT INTO spots (id, event_id, name, status, ticket_id)
+		VALUES (?, ?, ?, ?, ?)
+	`
+
+	_, err := r.db.Exec(query, spot.ID, spot.EventID, spot.Name, spot.Status, spot.TicketID)
+	return err
+}
+
+func (r *mysqlEventRepository) ReserveSpot(spotID, ticketID string) error {
+	query := `
+		UPDATE spots
+		SET status =?, ticket_id = ?
+		WHERE id = ?
+	`
+
+	_, err := r.db.Exec(query, domain.SpotStatusSold, ticketID, spotID)
+	return err
+}
+
+func (r *mysqlEventRepository) CreateTicket(ticket *domain.Ticket) error {
+	query := `
+		INSERT INTO tickets (id, event_id, spot_id, ticket_type, price)
+		VALUES (?, ?, ?, ?, ?)
+	`
+
+	_, err := r.db.Exec(query, ticket.ID, ticket.EventID, ticket.Spot.ID, ticket.TicketType, ticket.Price)
+	return err
+}
+
+func (r *mysqlEventRepository) FindEventByID(eventID string) (*domain.Event, error) {
+	query := `
+		SELECT id, name, location, organization, rating, date, image_url, capacity, price, partner_id
+		FROM events
+		WHERE id = ?
+	`
+	rows, err := r.db.Query(query, eventID)
+	if err != nil {
+		return nil, err
+	}
+	defer rows.Close()
+
+	var event *domain.Event
+	err = rows.Scan(
+		&event.ID,
+		&event.Name,
+		&event.Location,
+		&event.Organization,
+		&event.Rating,
+		&event.Date,
+		&event.ImageURL,
+		&event.Capacity,
+		&event.Price,
+		&event.PartnerID,
+	)
+	if err != nil {
+		return nil, err
+	}
+
+	return event, nil
+}
