3da44fa1cabde569d34a2e54b10447496dc27966
enrick-dev <enricksantos03@gmail.com>
2024-08-14 01:19:18 -0300

    atualizado domains

--- internal/events/domain/spot.go
+++ internal/events/domain/spot.go
@@ -7,13 +7,9 @@ import (
 )
 
 var (
-	ErrInvalidSpotNumber       = errors.New("invalid spot number")
-	ErrSpotNotFound            = errors.New("spot not found")
-	ErrSpotAlreadyReserved     = errors.New("spot already reserved")
-	ErrSpotNameTwoCharacters   = errors.New("spot name must be at least 2 characters")
-	ErrSpotNameRequired        = errors.New("spot name is required")
-	ErrSpotNameStartWithLetter = errors.New("spot name must start with a letter")
-	ErrSpotNameEndWithNumber   = errors.New("spot name must end with a number")
+	ErrInvalidSpotNumber   = errors.New("invalid spot number")
+	ErrSpotNotFound        = errors.New("spot not found")
+	ErrSpotAlreadyReserved = errors.New("spot already reserved")
 )
 
 type SpotStatus string
@@ -26,11 +22,12 @@ const (
 type Spot struct {
 	ID       string
 	EventID  string
-	Name     string
+	Name     string // all name uses the rule: Letter+Number. Ex: A1, B2, C3, etc.
 	Status   SpotStatus
 	TicketID string
 }
 
+// NewSpot creates a new spot with the given parameters.
 func NewSpot(event *Event, name string) (*Spot, error) {
 	spot := &Spot{
 		ID:      uuid.New().String(),
@@ -38,38 +35,36 @@ func NewSpot(event *Event, name string) (*Spot, error) {
 		Name:    name,
 		Status:  SpotStatusAvailable,
 	}
-
 	if err := spot.Validate(); err != nil {
 		return nil, err
-
 	}
 	return spot, nil
 }
 
+// Validate checks if the spot data is valid.
+func (s *Spot) Validate() error {
+	if len(s.Name) == 0 {
+		return errors.New("spot name is required")
+	}
+	if len(s.Name) < 2 {
+		return errors.New("spot name must be at least 2 characters long")
+	}
+	// Validate if the spot name is in the correct format
+	if s.Name[0] < 'A' || s.Name[0] > 'Z' {
+		return errors.New("spot name must start with a letter")
+	}
+	if s.Name[1] < '0' || s.Name[1] > '9' {
+		return errors.New("spot name must end with a number")
+	}
+	return nil
+}
+
+// Reserve reserves the spot for the given ticket ID.
 func (s *Spot) Reserve(ticketID string) error {
 	if s.Status == SpotStatusSold {
 		return ErrSpotAlreadyReserved
 	}
 	s.Status = SpotStatusSold
 	s.TicketID = ticketID
-	return nil
-}
-
-func (s Spot) Validate() error {
-	if s.Name == "" {
-		return ErrSpotNameRequired
-	}
-
-	if len(s.Name) < 2 {
-		return ErrSpotNameTwoCharacters
-	}
-
-	if s.Name[0] < 'A' || s.Name[0] > 'Z' {
-		return ErrSpotNameStartWithLetter
-	}
-
-	if s.Name[1] < '0' || s.Name[1] > '9' {
-		return ErrSpotNameEndWithNumber
-	}
 	return nil
 }
